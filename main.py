53b75fd8ed825cb007c12ac9f4b9080b435c20f5
William Douglas <wdoug7@gmail.com>
2020-07-24 01:36:44 +0000

    assigning variables

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
 from random import shuffle
 from itertools import product
-cards = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+J = 10
+Q = 10
+K = 10
+A = 11
+player_sum = 0
+dealer_sum = 0
+
+
+
+
+cards = (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A)
 suits = ('hearts','diamonds','spades','clubs')
 deck = list(product(cards,suits))
 
@@ -15,11 +27,17 @@ print("Dealing cards...")
 while len(deck) > 0:
  #deal player initial 2 cards
   player_hand = deck.pop() + deck.pop()
-  print (player_hand)
+  player_sum = player_hand[0] + player_hand[2]
+  print (player_hand[0],"of",player_hand[1])
+  print (player_hand[2],"of",player_hand[3])
+  print (f"Current Sum: {player_sum}")
   first = input("Hit or Stay? ")
   if first == "Hit":
     player_hand += deck.pop()
     print(player_hand)
+  if first == "Stay":
+    continue
+  
 
 
 print("Deck is Up!")
